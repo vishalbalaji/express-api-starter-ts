dc6276ef1fcb4df1054a79918a1fd2c4d5a420e7
Vishal Balaji D <vishalbalaji@arch>
2022-10-11 20:33:30 +0530

    :arrow_up_small: resolve merge conflicts with util-trpc

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import MessageResponse from '../interfaces/MessageResponse';
 import emojis from './emojis';
 import auth from './auth';
+
 import * as middlewares from '@/middlewares'
 
 const router = express.Router();
@@ -13,7 +14,8 @@ router.get<{}, MessageResponse>('/', (req, res) => {
   });
 });
 
-router.use('/auth', auth);
 router.use('/emojis', middlewares.isAuthenticated, emojis);
+
+router.use('/auth', auth);
 
 export default router;
